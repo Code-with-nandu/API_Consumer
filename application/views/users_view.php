165e29b70be3f6c6e0d7097251f06e9174b3e7ad
krishnendu dalui <krishnendudalui@gmail.com>
2024-09-12 12:47:55 +0530

    table display

--- application/views/users_view.php
+++ application/views/users_view.php
@@ -1,17 +1,52 @@
+
+<!DOCTYPE html>
 <html>
 <head>
     <title>Users List</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 <body>
     <h1>List of Users</h1>
-    <ul>
-        <?php if (!empty($users)) : ?>
-            <?php foreach ($users as $user) : ?>
-                <li><?php echo $user['id']; ?> (<?php echo $user['first_name']; ?>)<?php echo $user['last_name']; ?> (<?php echo $user['phone']; ?>)<?php echo $user['email']; ?></li>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <li>No users found.</li>
-        <?php endif; ?>
-    </ul>
+    <?php if (!empty($users)) : ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user) : ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($user['id']); ?></td>
+                        <td><?php echo htmlspecialchars($user['first_name']); ?></td>
+                        <td><?php echo htmlspecialchars($user['last_name']); ?></td>
+                        <td><?php echo htmlspecialchars($user['phone']); ?></td>
+                        <td><?php echo htmlspecialchars($user['email']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <p>No users found.</p>
+    <?php endif; ?>
 </body>
 </html>
