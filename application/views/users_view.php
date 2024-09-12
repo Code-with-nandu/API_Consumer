a66e21ecdc140b5b3cd49bb0c1eb0b545233e80f
krishnendu dalui <krishnendudalui@gmail.com>
2024-09-12 12:40:51 +0530

    Data dispaly using database

--- application/views/users_view.php
+++ application/views/users_view.php
@@ -7,7 +7,7 @@
     <ul>
         <?php if (!empty($users)) : ?>
             <?php foreach ($users as $user) : ?>
-                <li><?php echo $user['name']; ?> (<?php echo $user['email']; ?>)</li>
+                <li><?php echo $user['id']; ?> (<?php echo $user['first_name']; ?>)<?php echo $user['last_name']; ?> (<?php echo $user['phone']; ?>)<?php echo $user['email']; ?></li>
             <?php endforeach; ?>
         <?php else : ?>
             <li>No users found.</li>
